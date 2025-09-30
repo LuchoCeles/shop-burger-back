82f0e136101710d5a1cd0d840060690f8af3fa5f
Valentin Mendez <valentinmendez0042@gmail.com>
2025-09-29 21:54:01 -0300

    pedidos agregado al index.js

--- src/routes/index.js
+++ src/routes/index.js
@@ -2,11 +2,13 @@ const express = require('express');
 const authAdminRoutes = require('./authAdminRoutes');
 const productosRoutes = require('./productosRoutes');
 const categoriasRoutes = require('./categoriasRoutes');
+const pedidosRoutes = require('./pedidosRoutes');
 
 const router = express.Router();
 
 router.use('/admin', authAdminRoutes);
 router.use('/api', productosRoutes);
 router.use('/api', categoriasRoutes);
+router.use('/api', pedidosRoutes);
 
 module.exports = router;
