8140f5fb7609a317bdde7fd51eb949b8738ba758
Valentin Mendez <valentinmendez0042@gmail.com>
2025-10-16 13:44:59 -0300

    verificar delete de adicional

--- src/services/adicionalesService.js
+++ src/services/adicionalesService.js
@@ -1,4 +1,5 @@
-const { Adicionales } = require('../models');
+const { where } = require("sequelize");
+const { Adicionales, sequelize, AdicionalesXProducto } = require("../models");
 
 class AdicionalesService {
   async getAll(isActive = false) {
@@ -13,30 +14,49 @@ class AdicionalesService {
   async update(id, adicional) {
     const existingAdicional = await Adicionales.findByPk(id);
     if (!existingAdicional) {
-      throw new Error('Adicional no encontrado');
+      throw new Error("Adicional no encontrado");
     }
     return await existingAdicional.update(adicional);
   }
 
   async delete(id) {
-    const existingAdicional = await Adicionales.findByPk(id);
-    if (!existingAdicional) {
-      throw new Error('Adicional no encontrado');
+    const transaction = await sequelize.transaction();
+    try {
+      const existingAdicional = await Adicionales.findByPk(id, { transaction });
+      if (!existingAdicional) {
+        throw new Error("Adicional no encontrado");
+      }
+
+      const asociados = await AdicionalesXProducto.count({
+        where: { idAdicional: id }, //verificar
+        transaction,
+      });
+
+      if (asociados > 0) {
+        throw new Error(`No se puede eliminar, esta asociado a un producto.`);
+      }
+
+      await existingAdicional.destroy({ transaction });
+
+      await transaction.commit();
+
+      return { message: "Adicional eliminado correctamente" };
+      
+    } catch (error) {
+      if (!transaction.finished) await transaction.rollback();
+      throw new Error(`Error al eliminar adicional: ${error.message}`);
     }
-    await existingAdicional.destroy();
-    return { message: 'Adicional eliminado correctamente' };
   }
 
   async changeState(id) {
     const existingAdicional = await Adicionales.findByPk(id);
     if (!existingAdicional) {
-      throw new Error('Adicional no encontrado');
+      throw new Error("Adicional no encontrado");
     }
     existingAdicional.estado = !existingAdicional.estado;
     await existingAdicional.save();
     return existingAdicional;
   }
-
 }
 
 module.exports = new AdicionalesService();
