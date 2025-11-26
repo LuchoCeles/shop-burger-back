7b25910cbe4600a28d1e53e794217b05f5be0576
Lucho <lucianoceles@gmail.com>
2025-11-26 19:21:24 -0300

    arreglos en tam

--- src/controllers/tamController.js
+++ src/controllers/tamController.js
@@ -2,86 +2,90 @@ const { Result } = require("express-validator");
 const tamService = require("../services/tamService");
 
 class TamController {
-    
-    get = async (req, res, next) => {
-        try {
-            const tam = await tamService.get(); 
-            return res.status(200).json({
-                success: true,
-                message: "Tamaños de combos obtenidos",
-                data: tam
-            });
-        } catch (error) {
-            next(error);
-        }
+
+  get = async (req, res) => {
+    try {
+      const tam = await tamService.get();
+      return res.status(200).json({
+        success: true,
+        message: "Tamaños de combos obtenidos",
+        data: tam
+      });
+    } catch (error) {
+      return res.status(500).json({
+        success: false,
+        message: error.message
+      });
     }
+  }
 
-    create = async (req, res) => {
-        try {
-            const data = req.body;
-            const tam = await tamService.create(data);
-            return res.status(201).json({
-                success: true,
-                message: "Tamaño de 'combo' creado correctamente",
-                data: tam
-            });
-        } catch (error) {
-            return res.status(500).json({
-                success: false,
-                message: error.message
-            });
-        }
+  create = async (req, res) => {
+    try {
+      const { data } = req.body;
+
+      const tam = await tamService.create(data);
+      return res.status(201).json({
+        success: true,
+        message: "Tamaño de 'combo' creado correctamente",
+        data: tam
+      });
+    } catch (error) {
+      return res.status(500).json({
+        success: false,
+        message: error.message
+      });
     }
+  }
 
-    update = async (req,res,next) => {
-        try {
-            const {id} = req.params;
+  updateEstado = async (req, res, next) => {
+    try {
+      const { id } = req.params;
 
-            const tam =  await tamService.update(id);
+      const tam = await tamService.updateEstado(id);
 
-            return res.status(200).json({
-                success: true,
-                message: "Tamaño de combro modificado",
-                data: tam,
-            });
-        } catch (error) {
-            next(error);
-        }
+      return res.status(200).json({
+        success: true,
+        message: "Tamaño de combro modificado",
+        data: tam,
+      });
+    } catch (error) {
+      next(error);
     }
+  }
 
-    updateCategoria = async (req,res) =>{
-        try {
-            const {id} = req.params;
-            const data = req.body;
-            const tam = await tamService.updateCategoria(id,data);
-            return res.status(200).json({
-                success:true,
-                message: "Tamaño actualizado correctamente",
-                data:tam
-            });
-        } catch (error) {
-         return res.status(500).json({
-            success: false,
-            message: "Error al actualizar tamaño",
-         });   
-        }
+  updateTam = async (req, res) => {
+    try {
+      const { id } = req.params;
+      const { data } = req.body;
+      const tam = await tamService.updateTam(id, data);
+      return res.status(200).json({
+        success: true,
+        message: "Tamaño actualizado correctamente",
+        data: tam
+      });
+    } catch (error) {
+      return res.status(500).json({
+        success: false,
+        message: "Error al actualizar tamaño",
+      });
     }
+  }
 
-    delete = async (req,res) =>{
-        try {
-            const {id}  =req.params;
-            const tam = await tamService.delete(id);
-            return res.status(200).json({
-                success:true,
-                message: Result.message,
-            });
-        } catch (error) {
-            return res.status(500).json({
-                success:false,
-                message:"Error al eliminar tamaño",
-            });
-        }
+  delete = async (req, res) => {
+    try {
+      const { id } = req.params;
+      const tam = await tamService.delete(id);
+      return res.status(200).json({
+        success: true,
+        message: Result.message,
+      });
+    } catch (error) {
+      return res.status(500).json({
+        success: false,
+        message: "Error al eliminar tamaño",
+      });
     }
- }
+  }
+}
 
 module.exports = new TamController();
--- src/routes/tamRoutes.js
+++ src/routes/tamRoutes.js
@@ -9,12 +9,16 @@ const router = express.Router();
 router.get('/', TamController.get);
 
 router.post('/', authAdmin, validateRequest, [
-  body('nombre').notEmpty().withMessage('El nombre es obligatorio').isLength({ max: 25 }).withMessage('El nombre no puede tener más de 25 caracteres')
+  body('nombre').notEmpty().withMessage('El nombre es obligatorio').isLength({ max: 25 }).withMessage('El nombre no puede tener más de 25 caracteres'),
+  body('idCategoria').notEmpty().isInt({ min: 1 }).withMessage('La categoría es obligatoria'),
 ], TamController.create);
 
-router.patch('/:id/estado', authAdmin, TamController.update);
+router.patch('/:id',[
+  body('nombre').optional().notEmpty(),
+  body('idCategoria').optional().notEmpty()
+], authAdmin, TamController.updateTam);
 
-router.patch('/:id', authAdmin, TamController.updateCategoria);
+router.patch('/:id/estado', authAdmin, TamController.updateEstado);
 
 router.delete('/:id', authAdmin, TamController.delete);
 
