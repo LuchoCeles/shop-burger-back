46b060d477fb46db6d431d9efb3a0f0e8941cdf9
Valentin Mendez <valentinmendez0042@gmail.com>
2025-12-09 19:10:08 -0300

    delete horario

--- src/controllers/diasController.js
+++ src/controllers/diasController.js
@@ -65,6 +65,23 @@ class DiasController {
       });
     }
   }
+  async delete(req,res){
+    try {
+      const {id}= req.params;
+      const {horarios} = req.body;
+
+      const horarioEliminado = await diasService.delete(id,horarios);
+      return res.status(200).json({
+        success:true,
+        data: horarioEliminado,
+      });
+    } catch (error) {
+      return res.status(500).json({
+        success: false,
+        message: error.message
+      });
+    }
+  }
 }
 
 module.exports = new DiasController();
